ac1c33a0056a7094526cb8bab3ee2cbbd948bc82
yumehatdog <dezqpal@gmail.com>
2026-05-08 16:06:16 +0800

    Increase minimum window height from 380 to 390

--- watanays.PRM.lua
+++ watanays.PRM.lua
@@ -20,7 +20,7 @@ local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/dezqp
 
 local window = library:AddWindow(title, {
     main_color = Color3.fromRGB(0, 0, 0),
-    min_size = Vector2.new(420, 380),
+    min_size = Vector2.new(420, 390),
     can_resize = true,
 })
 
